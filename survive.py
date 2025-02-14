881d50e9103ccecb6f226285b7d603882f62fc04
Seong-Gi-hun <rhayanann60@gmail.com>
2025-02-14 14:35:16 +0530

    Update survive.py

--- survive.py
+++ survive.py
@@ -1,15 +1,10 @@
 import platform
 
 def survive():
-    
     id_numbers = [83, 74, 67, 69, 123, 121, 111, 117, 95, 103, 111, 116, 95, 105, 116, 125]
-  
     return ''.join(chr(x) for x in id_numbers)
 
-def is_online_platform():
-   
-    return platform.system() == "Linux"  
 
-
-if is_online_platform():
-    print(survive())  
+print("You are on the way to find the flag")
+print("\n".join(["."] *5))  # Prints 5 dots, each on a new line
+print(survive())
