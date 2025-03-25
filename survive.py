04471da3e23821285a2004de83891dc02cfebaf4
Seong-Gi-hun <rhayanann60@gmail.com>
2025-03-25 23:25:18 +0530

    Update survive.py

--- survive.py
+++ survive.py
@@ -1,10 +1,9 @@
 import platform
 
 def survive():
-    id_numbers = [83, 74, 67, 69, 123, 121, 111, 117, 95, 103, 111, 116, 95, 105, 116, 125]
+    id_numbers = [36, 74, 199, 8364, 45, 49, 84, 123, 52, 103, 51, 110, 116, 95, 107, 52, 49, 114, 48, 95, 48, 112, 51, 114, 52, 116, 49, 48, 110, 95, 115, 117, 99, 99, 51, 53, 53, 125]
     return ''.join(chr(x) for x in id_numbers)
 
-
 print("You are on the way to find the flag")
-print("\n".join(["."] *500)) 
+print("\n".join(["."] * 500)) 
 print(survive())
